359d4785cedcb6e01e877f881aa19ee171e23886
HossamMarey <hmarey@ymail.com>
2024-09-17 20:46:03 +0300

    changing app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div>
-      <Button> dddd </Button>
+      <Button> Hello </Button>
       <MainRoutes />
     </div>
   );
