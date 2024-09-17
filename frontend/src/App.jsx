fd2e939965bae44998ca70e5ee8aa7abc98fce48
HossamMarey <hmarey@ymail.com>
2024-09-17 20:42:52 +0300

    starting front end project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { MainRoutes } from "./MainRoutes";
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <div>
+      <Button> dddd </Button>
+      <MainRoutes />
+    </div>
+  );
+}
+
+export default App;
